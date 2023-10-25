536f4f265b7cf8b221adbf62044c76b47a8b5a46
Alyona567844 <alyona.busova@yandex.ru>
2023-10-25 23:06:34 +0500

    listtile

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -21,10 +21,57 @@ class HomeView extends GetView<HomeController> {
               TextButton(
                   onPressed: () => controller.getData(),
                   child: const Text("Get data")),
-              Obx(()=> Text(
-                controller.cars.length.toString(),
-                style: TextStyle(fontSize: 20),
-              )),
+              Obx(()=> ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: controller.cars.length,
+                  itemBuilder: (context, index) {
+                    var rolcar = controller.cars[index];
+                    return ListTile(
+                      leading: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(index.toString()),
+                        ],
+                      ),
+                      title: Row(children: [
+                        Text(
+                          rolcar.brand,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        Text(
+                          rolcar.model,
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 59, 58, 58)),
+                        )
+                      ]),
+                      subtitle: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              'Color: ${rolcar.color}',
+                              style: TextStyle(
+                                  color: const Color.fromARGB(255, 59, 58, 58)),
+                            ),
+                            Text(
+                              'Price: ${rolcar.price.toString()}',
+                              style: TextStyle(
+                                  color: const Color.fromARGB(255, 59, 58, 58)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          if (rolcar.availability)
+                            Icon(Icons.check_circle)
+                          else
+                            Icon(Icons.close)
+                        ],
+                      ),
+                    );
+                  })),
             ],
           ),
         ),
